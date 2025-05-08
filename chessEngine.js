320d40ed952fe3c5061df033b8337361546d3910
HugoW5 <hugo05.wakt@gmail.com>
2025-05-08 13:25:25 +0200

    minimaxing algorithm

--- chessEngine.js
+++ chessEngine.js
@@ -1,29 +1,103 @@
-var config = {
-    draggable: true,
-    dropOffBoard: 'snapback',
-    position: 'start',
-    onDrop: onDrop
+let board = null;
+let game = new Chess();
+
+function onDragStart(source, piece, position, orientation) {
+  if (game.game_over()) {
+    return false;
+  }
+  // only pick up pieces for White
+  if (piece.search(/^b/) !== -1) return false;
 }
 
-let board = Chessboard('board', config);
-var config = {
-    draggable: true,
-    dropOffBoard: 'snapback',
-    position: 'start',
-    onDrop: onDrop
+function minimax(depth, maximizingPlayer) {
+  if (depth == 0 || game.game_over()) {
+    return evaluateBoard();
+  }
+  if (maximizingPlayer) {
+    let maxEval = -Infinity;
+    for (let move of game.moves()) {
+      game.move(move);
+      let eval = minimax(depth - 1, false);
+      game.undo();
+      maxEval = Math.max(maxEval, eval);
+    }
+    return maxEval;
+  } else {
+    let minEval = Infinity;
+    for (let move of game.moves()) {
+      game.move(move);
+      let eval = minimax(depth - 1, true);
+      game.undo();
+      minEval = Math.min(minEval, eval);
+    }
+    return minEval;
+  }
 }
 
-let chess = new Chess(
-    board.fen(),
-  );
-console.log(board.fen());
+function evaluateBoard() {
+  const values = {
+    p: 1,
+    n: 3,
+    b: 3,
+    r: 5,
+    q: 9,
+    k: 0,
+  };
+  let score = 0;
+  const fen = game.fen().split(" ")[0];
 
-
-
-
-function onDrop(soruce, target, piece, ){
-console.log(soruce);
-console.log(target);
-console.log(piece);
-
+  for (let char of fen) {
+    if (/[a-z]/.test(char)) {
+      score -= values[char];
+    } else if (/[A-Z]/.test(char)) {
+      score += values[char.toLowerCase()];
+    }
+  }
+  return score;
 }
+
+function makeBestMove(depth = 1) {
+  let bestMove = null;
+  let bestValue = -Infinity;
+
+  for (let move of game.moves()) {
+    game.move(move);
+    let boardValue = minimax(depth - 1, false);
+    game.undo();
+    if (boardValue > bestValue) {
+      bestValue = boardValue;
+      bestMove = move;
+    }
+    console.log(boardValue);
+  }
+  console.log(bestMove);
+  game.move(bestMove);
+}
+
+function onDrop(source, target) {
+  let move = game.move({
+    from: source,
+    to: target,
+    promotion: "q", // always promote to a queen for example simplicity
+  });
+
+  // no legal moves
+  if (move === null) {
+    return "snapback";
+  }
+
+  makeBestMove(); // computers turn
+}
+
+function onSnapEnd() {
+  board.position(game.fen());
+}
+
+let config = {
+  draggable: true,
+  position: "start",
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd,
+};
+board = Chessboard("board", config);
